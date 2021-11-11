a77e1d37f6272065d4230667ad492fda56384e5c
Jonathan Lang <cryptoblockchainjunkie@gmail.com>
2021-11-11 10:42:41 -0500

    added and started the features of the shopping bag

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from "../../assets/dexagex.svg";
 
 import './header.style.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -25,12 +27,18 @@ const Header = ({ currentUser }) => (
                 : (
                     <Link className='option' to='/authent'>Sign In</Link>
                 )}
+            <CartIcon />
         </div>
+        {
+            hidden ? null :
+                <CartDropdown/>
+        }
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
